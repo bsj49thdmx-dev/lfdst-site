47242046a8e64de8fcf2daa30fa48726fdc4424d
bsj49thdmx-dev <bsj49thdmx@privaterelay.appleid.com>
2025-12-01 15:58:43 +0000

    Reparation style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,16 +4,16 @@ const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // C'est cette ligne qui trouve tes nouveaux dossiers !
   ],
   theme: {
     extend: {
       colors: {
-        gold: "#D4AF37", // Ton doré exact
+        gold: "#D4AF37",
       },
       fontFamily: {
-        serif: ['var(--font-playfair)', 'serif'], // Pour les Titres
-        sans: ['var(--font-montserrat)', 'sans-serif'], // Pour le texte
+        serif: ['var(--font-playfair)', 'serif'],
+        sans: ['var(--font-montserrat)', 'sans-serif'],
       },
     },
   },
